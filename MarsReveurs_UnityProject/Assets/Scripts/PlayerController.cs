1b178a91a749fcbec906903100d4c0fb92585c42
François Ségaud <fseg@opmbx.org>
2018-01-27 20:02:06 +0100

    Debug moves and Nop.

--- MarsReveurs_UnityProject/Assets/Scripts/PlayerController.cs
+++ MarsReveurs_UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,13 @@
 ﻿public class PlayerController : UnityEngine.MonoBehaviour, IRoverInterface
 {
+    [UnityEngine.SerializeField]
+    private UnityEngine.Tilemaps.Tilemap tilemap;
+
+    public void Nop()
+    {
+        UnityEngine.Debug.Log("Noping...");
+    }
+
     public void Rotate(Direction direction)
     {
         UnityEngine.Debug.Log("Rotating to " + direction + "...");
@@ -7,9 +15,27 @@
 
     void Start ()
     {
-	}
+    }
 	
 	void Update ()
     {
-	}
+#if UNITY_EDITOR
+        if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.UpArrow))
+        {
+            this.transform.position += UnityEngine.Vector3.up;
+        }
+        else if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.DownArrow))
+        {
+            this.transform.position += UnityEngine.Vector3.down;
+        }
+        else if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.LeftArrow))
+        {
+            this.transform.position += UnityEngine.Vector3.left;
+        }
+        else if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.RightArrow))
+        {
+            this.transform.position += UnityEngine.Vector3.right;
+        }
+#endif
+    }
 }
