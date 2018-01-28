41a7aaca195e0a3e108422617da96eeb1c3e85c8
François Ségaud <fseg@opmbx.org>
2018-01-28 01:07:01 +0100

    Energy and collisions

--- MarsReveurs_UnityProject/Assets/Scripts/PlayerController.cs
+++ MarsReveurs_UnityProject/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,17 @@ public class PlayerController : UnityEngine.MonoBehaviour, IRoverInterface
     [UnityEngine.SerializeField]
     private UnityEngine.Tilemaps.Tilemap tilemap;
 
+    public float Energy
+    {
+        get;
+        private set;
+    }
+
+    public void Awake()
+    {
+        this.Energy = 1f;
+    }
+
     public void Nop()
     {
         this.MoveForward();
@@ -40,11 +51,30 @@ public class PlayerController : UnityEngine.MonoBehaviour, IRoverInterface
         this.transform.position += this.transform.up;
     }
 
-    void Start ()
+    public void SlapTheCuteRover(float energyLoss)
     {
+        if (energyLoss < 0f)
+        {
+            UnityEngine.Debug.LogWarning("The damage value is supposed to be positive you idiot!");
+            return;
+        }
+
+        this.Energy = UnityEngine.Mathf.Clamp01(this.Energy - energyLoss);
+
+        if (UnityEngine.Mathf.Abs(this.Energy) < float.Epsilon)
+        {
+            this.Die(DeathReason.ZeroEnergy);
+        }
     }
-	
-	void Update ()
+
+    public void Die(DeathReason reason = DeathReason.YouDeserveIt)
+    {
+        this.Energy = 0f;
+
+        UnityEngine.Debug.Log("You died because " + reason);
+    }
+
+    void Update ()
     {
 		this.Camera.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.Camera.position.z);
 
@@ -61,6 +91,18 @@ public class PlayerController : UnityEngine.MonoBehaviour, IRoverInterface
         {
             this.MoveForward();
         }
+
+        //UnityEngine.Debug.Log(this.transform.position);
 #endif
+    }
+
+    private void OnGUI()
+    {
+        UnityEngine.GUI.Label(new UnityEngine.Rect(10f, 10f, 100f, 25f), this.Energy.ToString("0.00"));
+    }
+
+    void OnTriggerEnter2D(UnityEngine.Collider2D coll)
+    {
+        this.SlapTheCuteRover(.1f);
     }
 }
